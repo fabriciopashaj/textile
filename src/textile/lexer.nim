1b1e162f31d2948448d467d649e010875822bf0b
fabriciopashaj <fabriciopashaj.dev@gmail.com>
2023-09-21 17:38:32 +0300

    feat(lexer): Implemented basic integer, float and identifier lexing.
    Also added tests.

--- src/textile/lexer.nim
+++ src/textile/lexer.nim
@@ -1,4 +1,4 @@
-import lexbase, unicode, strutils, strformat
+import lexbase, unicode, strutils, strformat, sugar
 import yarolin/results
 import ./util
 
@@ -12,24 +12,30 @@ type
 
   TokenKind* {.pure.} = enum
     Eof
-    StrLit = "$strlit"
+    StrLit    = "$strlit"
+    IntLit    = "$int"
+    FloatLit  = "$float"
+    Ident     = "$ident"
   Token* = object
     span*: Span
     case kind*: TokenKind
     of Eof:
       discard
-    of StrLit:
+    of StrLit, Ident:
       str*: string
+    of IntLit:
+      integer*: BiggestInt
+    of FloatLit:
+      real*: BiggestFloat
 
   LexError* {.pure.} = enum
     NotHexDigit
     UntermiatedString
   Lexer* {.byRef.} = object of BaseLexer
-    inited: bool = false
     rune: Rune
     token: Token
 
-func `..`(start, finish: Pos): Span =
+func `..`(start, finish: Pos): Span {.inline.} =
   result.line = start.line..finish.line
   result.col = start.col..finish.col
   result.offset = start.offset..finish.offset
@@ -38,24 +44,30 @@ using
   L: Lexer
   lexer: var Lexer
 
-func position*(L; offset = 0): Pos {.inline.} =
-  (L.lineNumber, L.getColNumber(L.bufpos + offset), L.offsetBase + L.bufpos - 1)
+func `<=`(a, b: Rune): bool {.inline.} = int32(a) <= int32(b)
+func `>=`(a, b: Rune): bool {.inline.} = int32(a) >= int32(b)
 
-proc nextChar(lexer): Rune =
-  fastRuneAt(lexer.buf, lexer.bufpos, lexer.rune, false)
+func position*(L; offset = 0): Pos {.inline.} =
+  (L.lineNumber,
+   L.getColNumber(L.bufpos + offset),
+   L.offsetBase + L.bufpos + offset)
+
+proc nextRune(lexer): Rune =
+  var pos = lexer.bufpos
+  fastRuneAt(lexer.buf, pos, lexer.rune)
   if lexer.rune == Rune'\n':
     lexer.bufpos = lexer.handleLF(lexer.bufpos)
   elif lexer.rune == Rune'\c':
     lexer.bufpos = lexer.handleCR(lexer.bufpos)
   else:
-    inc lexer.bufpos, size(lexer.rune)
+    lexer.bufpos = pos # inc lexer.bufpos, size(lexer.rune)
   result = lexer.rune
 
 proc skip(lexer; charset: set[char]) =
   while true:
     if size(lexer.rune) != 1 or char(lexer.rune) notin charset:
       break
-    discard lexer.nextChar()
+    discard lexer.nextRune()
 
 func hexValue(hexDigit: char): !!int {.inline.} =
   if   hexDigit in '0'..'9': result =!+ ord(hexDigit) - ord('0')
@@ -63,12 +75,12 @@ func hexValue(hexDigit: char): !!int {.inline.} =
   elif hexDigit in 'A'..'Z': result =!+ ord(hexDigit) - ord('A') + 0xa
   else                     : result =!- NotHexDigit
 
-proc lexEscapeSeq(lexer): LexError!bool =
-  case lexer.nextChar()
+proc lexEscapeSeq(lexer): !!bool =
+  case lexer.nextRune()
   of Rune'x':
     let
-      digit1 = hexValue(char lexer.nextChar()).try
-      digit2 = hexValue(char lexer.nextChar()).try
+      digit1 = hexValue(char lexer.nextRune()).try
+      digit2 = hexValue(char lexer.nextRune()).try
     lexer.rune = Rune((digit1 shl 8) or digit2)
   of Rune'n':
     lexer.rune = Rune'\l'
@@ -80,32 +92,100 @@ proc lexEscapeSeq(lexer): LexError!bool =
     todo("other escape sequences")
   result =!+ false
 
-proc lexStrLit(lexer): !!string =
-  var acc = newStringOfCap(16)
+proc lexStrLit(lexer): !!(string, Pos) =
+  # NOTE: We have to do some goofy stuff in here, like storig the position
+  #       before we skip a rune, so that we don't lose track of the end of the
+  #       token on newlines. Maybe a better way would be to have smth like a 1
+  #       rune lookahead.
+  discard lexer.nextRune() # skip '"'
+  var
+    acc = ""
+    pos = lexer.position()
   while true:
-    case lexer.nextChar()
+    case lexer.rune
     of Rune'\\':
       if lexer.lexEscapeSeq().try:
         continue
     of Rune'"':
+      discard lexer.nextRune()
       break
+    of Rune(EndOfFile):
+      returnErr UntermiatedString
     else: discard
     acc.add(lexer.rune)
-  result =!+ acc
+    pos = lexer.position()
+    discard lexer.nextRune()
+  result =!+ (acc, pos)
+
+proc lexNumber(lexer; start: Pos): !!Token =
+  var
+    token = Token(kind: IntLit, span: start..start)
+    intAcc = 0.BiggestInt
+    pos: Pos
+  while lexer.rune in Rune('0')..Rune('9'):
+    pos = lexer.position()
+    intAcc *= 10
+    inc intAcc, ord(lexer.rune) - ord('0')
+    discard lexer.nextRune()
+  if lexer.rune != Rune'.':
+    token.span.line.b = pos.line
+    token.span.col.b = pos.col
+    token.span.offset.b = pos.offset
+    token.integer = intAcc
+    returnVal token
+  # TODO: Add scientific notation format and other bases.
+  # XXX: Doesnt't check if there's a dot here, assumes
+  discard lexer.nextRune()
+  var
+    n = 0
+    decimal = 0
+  while lexer.rune in Rune('0')..Rune('9'):
+    pos = lexer.position()
+    decimal *= 10
+    inc decimal, ord(lexer.rune) - ord('0')
+    n *= 10
+    discard lexer.nextRune()
+  token.span.line.b = pos.line
+  token.span.col.b = pos.col
+  token.span.offset.b = pos.offset
+  returnVal Token(
+    kind: FloatLit,
+    span: token.span,
+    real: BiggestFloat(intAcc) + BiggestFloat(decimal) / BiggestFloat(n))
+
+proc lexIdent(lexer): (string, Pos) =
+  var
+    acc = ""
+    pos: Pos
+  while size(lexer.rune) > 1 or char(lexer.rune) in IdentChars:
+    pos = lexer.position()
+    acc.add(lexer.rune)
+    discard lexer.nextRune()
+  result = (acc, pos)
 
 proc next*(lexer): !!Token =
-  if lexer.inited:
-    result =!+ lexer.token
-  else:
+  if lexer.offsetBase + lexer.bufpos == 0:
     result =!+ Token(kind: Eof)
-    discard lexer.nextChar()
+    discard lexer.nextRune()
+  elif lexer.token.kind == Eof:
+    returnVal lexer.token
+  result =!+ lexer.token
   lexer.skip(Whitespace)
-  let start = lexer.position()
-  case lexer.rune
-  of Rune'"':
-    returnVal Token(
-      kind: StrLit,
-      str: lexer.lexStrLit().try,
-      span: start..lexer.position())
+  let start = lexer.position(-1)
+  if size(lexer.rune) > 1: # NOTE: just assumes it is an identifier for now
+    let (str, endPos) = lexer.lexIdent()
+    lexer.token = Token(kind: Ident, str: str, span: start..endPos)
+    return
+  case char(lexer.rune)
+  of '"':
+    let (str, endPos) = lexer.lexStrLit().try
+    lexer.token = Token(kind: StrLit, str: str, span: start..endPos)
+  of '0'..'9':
+    lexer.token = lexer.lexNumber(start).try
+  of 'a'..'z', 'A'..'Z':
+    let (str, endPos) = lexer.lexIdent()
+    lexer.token = Token(kind: Ident, str: str, span: start..endPos)
+  of EndOfFile:
+    lexer.token = Token(kind: Eof, span: lexer.position()..lexer.position())
   else:
     todo(fmt"Other kinds of tokens: {lexer.rune=}")
--- tests/test_lexer.nim
+++ tests/test_lexer.nim
@@ -1,4 +1,4 @@
-import unittest, streams
+import unittest, streams, math
 import ../src/textile/lexer
 import yarolin/results
 
@@ -6,22 +6,56 @@ suite "lexer":
   setup:
     var lexer: Lexer
   test "string literals":
-    lexer.open(newStringStream("\"foo\n bar\" \"fr\"\n\"\\x0a\\n\""))
+    lexer.open(newStringStream("\"foo\n bar\" \"fr\"\n\"\\x0c\\n\""))
+    discard lexer.next.getVal() # first call for initialization
     var token = lexer.next().getVal()
     check token.kind == StrLit
     check token.str == "foo\n bar"
     check token.span.line == 1..2
-    check token.span.col == 1..5
+    check token.span.col == 0..4
     check token.span.offset == 0..9
     token = lexer.next().getVal()
     check token.kind == StrLit
     check token.str == "fr"
     check token.span.line == 2..2
-    check token.span.col == 7..10
+    check token.span.col == 6..9
     check token.span.offset == 11..14
     token = lexer.next().getVal()
     check token.kind == StrLit
-    check token.str == "\x0a\n"
+    check token.str == "\x0c\n"
     check token.span.line == 3..3
-    check token.span.col == 1..8
+    check token.span.col == 0..7
     check token.span.offset == 16..23
+  test "integers":
+    lexer.open(newStringStream("123"))
+    discard lexer.next.getVal() # first call for initialization
+    var token = lexer.next().getVal()
+    check token.kind == IntLit
+    check token.integer == 123
+    check token.span.line == 1..1
+    check token.span.col == 0..3
+    check token.span.offset == 0..3
+  test "floats":
+    lexer.open(newStringStream("456.75"))
+    discard lexer.next.getVal() # first call for initialization
+    var token = lexer.next().getVal()
+    check token.kind == FloatLit
+    check token.real.almostEqual(456.75)
+    check token.span.line == 1..1
+    check token.span.col == 0..6
+    check token.span.offset == 0..6
+  test "identifiers":
+    lexer.open(newStringStream("fooBar_baz a0323bcs"))
+    discard lexer.next.getVal() # first call for initialization
+    var token = lexer.next().getVal()
+    check token.kind == Ident
+    check token.str == "fooBar_baz"
+    check token.span.line == 1..1
+    check token.span.col == 0..10
+    check token.span.offset == 0..10
+    token = lexer.next().getVal()
+    check token.kind == Ident
+    check token.str == "a0323bcs"
+    check token.span.line == 1..1
+    check token.span.col == 11..19
+    check token.span.offset == 11..19
